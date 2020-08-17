8833ea9062c7f81cd128f7d90b52b6806e0c669d
faizan <faizanshafqat519@gmail.com>
2020-08-18 00:27:16 +0500

    Updates

---
+++ precache-manifest.441e0a117d6cf26dcf693f6cdfc7490f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd9c962c087ffac0244f762e71a74071",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "4812451bf31e05182ba6",
+    "url": "/monster-rolodex/static/css/main.e4effa07.chunk.css"
+  },
+  {
+    "revision": "f89c3c7743c6af4940d9",
+    "url": "/monster-rolodex/static/js/2.91579c33.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.91579c33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4812451bf31e05182ba6",
+    "url": "/monster-rolodex/static/js/main.cc6ee7d9.chunk.js"
+  },
+  {
+    "revision": "945ec1fc1a6e6d1ede6a",
+    "url": "/monster-rolodex/static/js/runtime-main.860af898.js"
+  }
+]);
